718bb52a104688ce05afe3075068a81e3cb36cc3
fateme <fatemeyousefi9675@gmail.com>
2024-05-27 20:05:02 +0330

    2nd Home Work

---
+++ HW2.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+public class HW2 {
+    public static void main(String[] args) {
+        String name = "Fateme Yousefi";
+        byte age = 27;
+        int Height = 150;
+        float weight = 49.5f;
+        boolean Married = false;
+        char Gender='F';
+        long PhoneNumber = 123456789065465342l;
+        short YearOfBirth=1996;
+
+       System.out.println("Name: "+name+"\n"+ "YearOfBirth: "+YearOfBirth+"\n"+
+               "Age:"+age+"\n"+ "Height: "+Height+"\n"+"Weight: "+weight+"\n"+
+                "Gender :"+Gender+"\n"+ "Married: "+Married+"\n"+"PhoneNumber: "+PhoneNumber);
+    }
+}
